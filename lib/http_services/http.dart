b0fc162ff7474520194c9f735c7a232afdc81628
Yayo Acosta <yayo.ct@gmail.com>
2018-12-24 23:22:34 +0100

    Solve HTTP response getting truncated

--- lib/http_services/http.dart
+++ lib/http_services/http.dart
@@ -4,7 +4,10 @@ import 'dart:convert';
 httpGet(String url) async {
   var request = await HttpClient().getUrl(Uri.parse(url));
   var response = await request.close();
-  await for (var payload in response.transform(Utf8Decoder())) {
-    return json.decode(payload);
+  //var object = json.decode(response.transform(utf8.decoder).join());
+  String payload = '';
+  await for (String chunk in response.transform(Utf8Decoder())) {
+    payload += chunk;
   }
+  return json.decode(payload);
 }
